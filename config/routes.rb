69a1e14dbcc305e7cc8279bfc6d218f22b346389
Joshua Kemp <josh.kemp@ziplist.com>
2014-10-15 17:06:12 -0400

    Generating the basic Rails app, calling Google Api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 GeoLocate::Application.routes.draw do
   resources :locations
 
+    # root :to => 'locations#show'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +53,7 @@ GeoLocate::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
